9f88d9da5761c42c58a6699d32bff92b89d3f370
Aluno <etecae023@hotmail.com>
2025-02-12 21:21:03 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  constructor() {}
+
+  n1=0;
+  n2=0;
+  res=0;
+  
+
+}
